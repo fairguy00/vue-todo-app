bc8f351d5f62b451774716832096c804cf6ed4f5
fairguy00 <thbusae@gmail.com>
2021-10-03 15:31:36 +0900

    주소파람에 따른 라우팅
    $router, $route설정

--- App.vue
+++ App.vue
@@ -9,6 +9,14 @@
     <router-view/>
   </div>
 </template>
+<script>
+export default {
+  created(){
+    console.log('router :'+this.$router)//우리가 세팅해놓은 라우트정보들
+    console.log('route :'+this.$route)//주소의 쿼리스트링을 쿼리객체로 참조함
+  }
+}
+</script>
 <style lang="scss">
  .to-home.router-link-exact-active{//router-link 에서현재페이지 아닌지 확인
    display: none;
