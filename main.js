33906b5285a742562d384ed6923738c39c2469ad
fairguy00 <thbusae@gmail.com>
2021-10-05 14:24:40 +0900

    store 사용하기위한 Vuex 설치

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 import App from './App'
 // import {createApp} from 'vue'
 import router from './router/index.js' //index.js는 생략가능
-
+import store from './store/index.js'
 // export const eventBus = createApp(App)
 
 // createApp(App).mount('#app')
@@ -14,8 +14,10 @@ new Vue({
   //render (createElement) { //createElement 콜백함수
   //  return createElement(App)
   //}//아래두가지와 같다
+  store,
   render: h => h(App) //에로우펑션에서 h는 createElement의 축약형
   //render:(h) => {
   //  return h(App)
   //}
+
 })// main.js 진입 -> App.vue(최상위 컴포넌트) 로 한번만 연결 -> 나머지 컴포넌트들과 연결
